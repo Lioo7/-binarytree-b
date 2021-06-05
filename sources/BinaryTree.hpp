4d956634ad5f448e452522b995af88e31bebe8de
Lioo7 <lioz.akirav@gmail.com>
2021-06-05 22:27:05 +0300

    update adds

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-
+using namespace std;
 namespace ariel
 {
     /*====================================Implementation-Of-A-Generic-Binary-Tree=============================*/
@@ -10,7 +10,7 @@ namespace ariel
         /*===========================================Inner-Node-Class=========================================*/
         class Node
         {
-        public:          // TODO: public?
+        public:
             T value;     // the value of the node
             Node *left;  // left child
             Node *right; //right child
@@ -23,32 +23,36 @@ namespace ariel
             }
             // destructor
             ~Node();
+
         };
 
         /*===========================================Private-Methods=========================================*/
 
-        Node *root;                        // the root of the tree
-        // std ::multimap<T, Node *> my_tree; // contains all the nodes in the tree
+        Node *root;                  // the root of the tree
+        multimap<T, Node *> my_tree; // contains all the nodes in the tree
 
         // This function search the given node value in the tree and return a poniter to its node
         Node *search_node(T target_value)
         {
-            Node *target_node = nullptr;
-            // if (this->root == nullptr)
-            // {
-            //     // the tree is empty
-            //     cout << "the tree is empty" << endl;
-            // }
-            // else
-            // {
-            //     // traverse through all the nodes in the tree
-            //     // TODO: add code here
-            //     // node found
-            //     cout << "the node is found" << endl;
-            //     // node is not found
-            //     cout << "the node is notfound" << endl;
-            // }
+            Node target_node;
+            // the tree is empty
+            if (this->root == nullptr)
+            {
+                target_node = nullptr;
+            }
+            // the tree is not empty
+            else
+            {
+                target_node = my_tree.find(target_value);
+            }
+
             return target_node;
+        }
+
+        void add_to_map(T node_value, Node *node_pointer)
+        {
+            //inserts the node into the multimap
+            my_tree.insert({node_value, node_pointer});
         }
 
         /*===========================================Public-Methods=========================================*/
@@ -59,22 +63,26 @@ namespace ariel
         {
             this->root = nullptr;
         }
+        // copy constructor
+        
 
         /*===========================================Insert-Methods=========================================*/
 
         // gets an input and puts it at the root of the tree
         BinaryTree<T> &add_root(T new_root_value)
         {
-            // // empty tree
-            // if (root == nullptr)
-            // {
-            //     root = new Node(new_root_value);
-            // }
-            // // there is a root already
-            // else
-            // {
-            //     root->value = new_root_value;
-            // }
+            // empty tree
+            if (root == nullptr)
+            {
+                root = new Node(new_root_value);
+            }
+            // there is a root already
+            else
+            {
+                root->value = new_root_value;
+            }
+            //inserts the root into the multimap
+            add_to_map(new_root_value, root);
 
             return *this;
         }
@@ -82,14 +90,30 @@ namespace ariel
         // adds a new node to be the left child of the exist given parent
         BinaryTree<T> &add_left(T parent, T left_child)
         {
-            // // the tree is empty
-            // if (root == nullptr)
-            // {
-            //     throw std::invalid_argument("Error: can't add this element beacuse the tree is empty");
-            // }
-            // Node n_parent;
-            // ;
-            // // this node already exist
+            // the tree is empty
+            if (root == nullptr)
+            {
+                throw std::invalid_argument("Error: can't add this element beacuse the tree is empty");
+            }
+
+            Node p = search_node(parent);
+            // parent does not exist
+            if (p == nullptr)
+            {
+                throw std::invalid_argument("Error: the parent does not exist in the tree");
+            }
+            // the parent has not a left child
+            else if (p->left == nullptr)
+            {
+                p->left = new Node(left_child);
+                //inserts the new node into the multimap
+                add_to_map(left_child, p->left);
+            }
+            // the parent already has a left child
+            else
+            {
+                p->left->value = left_child;
+            }
 
             return *this;
         }
@@ -97,13 +121,31 @@ namespace ariel
         // adds a new node ot be the right child of the exist given parent
         BinaryTree<T> &add_right(T parent, T right_child)
         {
-            // Node *p = nullptr;
-            // p = search_node(parent);
-            // if (p != nullptr)
-            // {
-            //     Node *child = Node(right_child); // TODO: ->
-            //     p->right = child;
-            // }
+            // the tree is empty
+            if (root == nullptr)
+            {
+                throw std::invalid_argument("Error: can't add this element beacuse the tree is empty");
+            }
+
+            Node p = search_node(parent);
+            // parent does not exist
+            if (p == nullptr)
+            {
+                throw std::invalid_argument("Error: the parent does not exist in the tree");
+            }
+            // the parent has not a right child
+            else if (p->right == nullptr)
+            {
+                p->right = new Node(right_child);
+                //inserts the new node into the multimap
+                add_to_map(right_child, p->right);
+            }
+            // the parent already has a right child
+            else
+            {
+                p->right->value = right_child;
+            }
+
             return *this;
         }
 
